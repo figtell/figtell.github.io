0ee2b2fb0e52507e3538205b05bb56fad3e77a9b
Michael Huang <michael@UMichael.local>
2018-03-05 07:04:21 +0800

    test image upload again

--- js/main.js
+++ js/main.js
@@ -1,37 +1,38 @@
-var cv = document.getElementById('cvHead'),
-ctx = cv.getContext('2d'),
-W,
-H,
-dataURI;
+/*var cv = document.getElementById('cvHead'),
+  ctx = cv.getContext('2d'),
+  W,
+  H,
+  dataURI;
 
 function drawFile(){
   var imageFile = new Image();
-  if(dataURI){
-    imageFile.src = dataURI;
-    W = cv.width = imageFile.width;
-    H = cv.height = imageFile.height;
-    imageFile.onload = function(){
-      ctx.clearRect(0, 0, W, H);
-      ctx.drawImage(imageFile, 0, 0);
-    };
-  }
+    if(dataURI){
+      imageFile.src = dataURI;
+      W = cv.width = imageFile.width;
+      H = cv.height = imageFile.height;
+      imageFile.onload = function(){
+        ctx.clearRect(0, 0, W, H);
+        ctx.drawImage(imageFile, 0, 0);
+      };
+    }
 }
 
 function init(){
-  $('#upload').on('change', upload);
+  $('#upimg').on('change', upload);
 }
 
 function upload(){
+  alert("init");
   if(window.File){
     var reader = new FileReader(),
-    file = this.files[0];
-    if(!this.files.length){
-      return;
-    }
-    if(!file.type.match(/image\/\w+/)){
-      alert('Please choose image');
-      return;
-    }
+      file = this.files[0];
+        if(!this.files.length){
+          return;
+        }
+        if(!file.type.match(/image\/\w+/)){
+          alert('Please choose image');
+          return;
+        }
 
     reader.readAsDataURI(file);
     reader.onload = function(e){
@@ -39,4 +40,21 @@ function upload(){
       drawFile();
     };
   }
+}*/
+
+function readURL(input) {
+
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      $('#blah').attr('src', e.target.result);
+    }
+
+    reader.readAsDataURL(input.files[0]);
+  }
 }
+
+$("#imgInp").change(function() {
+  readURL(this);
+});
