4d773d558a39572052a962a564b253a460f1a277
Michael Huang <michael@UMichael.local>
2018-03-06 06:59:25 +0800

    test modified js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
-/*var cv = document.getElementById('cvHead'),
-  ctx = cv.getContext('2d'),
-  W,
-  H,
-  dataURI;
+(function($){
+  'use strict';
 
-function drawFile(){
-  var imageFile = new Image();
-    if(dataURI){
-      imageFile.src = dataURI;
+  var cv = document.getElementById('cvHead'),
+    ctx = cv.getContext('2d'),
+    W,
+    H,
+    dataURL;
+
+  $(document).ready(function(){
+        init();
+    });
+
+  function drawFile(){
+    var imageFile = new Image();
+
+    if(dataURL){
+      imageFile.src = dataURL;
       W = cv.width = imageFile.width;
       H = cv.height = imageFile.height;
       imageFile.onload = function(){
@@ -15,46 +23,47 @@ function drawFile(){
         ctx.drawImage(imageFile, 0, 0);
       };
     }
-}
-
-function init(){
-  $('#upimg').on('change', upload);
-}
-
-function upload(){
-  alert("init");
-  if(window.File){
-    var reader = new FileReader(),
-      file = this.files[0];
-        if(!this.files.length){
-          return;
-        }
-        if(!file.type.match(/image\/\w+/)){
-          alert('Please choose image');
-          return;
-        }
-
-    reader.readAsDataURI(file);
-    reader.onload = function(e){
-      dataURI = e.target.result;
-      drawFile();
-    };
   }
-}*/
 
-function readURL(input) {
+  function init(){
+    $('#upimg').on('change', upload);
+  }
 
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
+  function upload(){
+    alert("init");
+    if(window.File){
+      var reader = new FileReader(),
+        file = this.files[0];
+          if(!this.files.length){
+            return;
+          }
+          if(!file.type.match(/image\/\w+/)){
+            alert('Please choose image');
+            return;
+          }
 
-    reader.onload = function(e) {
-      $('#blah').attr('src', e.target.result);
+      reader.readAsDataURL(file);
+      reader.onload = function(e){
+        dataURL = e.target.result;
+        drawFile();
+      };
     }
-
-    reader.readAsDataURL(input.files[0]);
   }
-}
 
-$("#imgInp").change(function() {
-  readURL(this);
-});
+  function readURL(input) {
+
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function(e) {
+        $('#blah').attr('src', e.target.result);
+      }
+
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  $("#imgInp").change(function() {
+    readURL(this);
+  });
+})(jQuery);
