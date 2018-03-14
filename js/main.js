21dc634045919c1975ccd85657f2461ce39355a2
Michael Huang <michael@UMichael.local>
2018-03-15 00:54:02 +0800

    layout and UX adjustment

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@
     ctx = cv.getContext('2d'),
     cvb = document.getElementById('cvBox'),
     ctb = cvb.getContext('2d'),
+    cvt = document.getElementById('cvTop'),
+    ctt = cvt.getContext('2d'),
     line = document.getElementById('lineText'),
     W,
     Wb,
@@ -13,29 +15,52 @@
     dataURL,
     inLine = document.getElementById('inputLine'),
     textArea = document.getElementById('lineTextArea'),
-    lineArray = [];
+    lineArray = [],
+    timer = null;
+
+  $('#btnLineSet').click(function(){
+    lineArray.push(inLine.value);
+    //alert(lineArray);
+    if(textArea.value){
+      textArea.value = textArea.value+"\n"+inLine.value;
+    }else{
+      textArea.value = inLine.value;
+    }
+    inLine.value = "";
+    inLine.focus();
+  });
+
+  $('#inputLine').keyup(function(event){
+    if(event.keyCode === 13){
+      $('#btnLineSet').click();
+    }
+  });
+
+  $('#btnSet').click(function(){
+    document.getElementById('nameText').innerHTML=document.getElementById('inputName').value;
+    document.getElementById('form').style.display = "block";
+    document.getElementById('inpForm').style.display = "block";
+    inLine.focus();
+  });
+
+  $('#inputPlace').keyup(function(event){
+    if(event.keyCode === 13){
+      $('#btnSet').click();
+    }
+  });
 
   $(document).ready(function(){
         var imageFile = new Image();
         imageFile.src = "img/talentB.jpg";
         imageFile.onload = function(){
-          Wb = cvb.width = imageFile.width;
-          Hb = cvb.height = imageFile.height;
+          Wb = cvt.width = cvb.width = imageFile.width;
+          Hb = cvt.height = cvb.height = imageFile.height;
           ctb.clearRect(0, 0, Wb, Hb);
+          ctt.clearRect(0, 0, Wb, Hb);
           ctb.drawImage(imageFile, 0, 0);
         };
 
         textArea.value = "";
-        $('#btnLineSet').click(function(){
-          lineArray.push(inLine.value);
-          //alert(lineArray);
-          if(textArea.value){
-            textArea.value = textArea.value+"\n"+inLine.value;
-          }else{
-            textArea.value = inLine.value;
-          }
-          inLine.value = "";
-        });
         init();
     });
 
@@ -55,14 +80,18 @@
 
   function init(){
     $('#upimg').on('change', upload);
-    $('#cvBox').on('mousedown', clicked);
+    /*$('#cvBox').on('mousedown', clicked);
     $('#cvBox').on('mouseup', released);
     $('#cvBox').on('touchstart', clicked);
     $('#cvBox').on('touchend', released);
     $('#cvHead').on('mousedown', clicked);
     $('#cvHead').on('mouseup', released);
     $('#cvHead').on('touchstart', clicked);
-    $('#cvHead').on('touchend', released);
+    $('#cvHead').on('touchend', released);*/
+    $('#cvTop').on('mousedown', clicked);
+    $('#cvTop').on('mouseup', released);
+    $('#cvTop').on('touchstart', clicked);
+    $('#cvTop').on('touchend', released);
   }
 
   function clicked(){
@@ -82,7 +111,11 @@
       var cnt = Math.floor(Math.random()*lineArray.length);
       //alert(cnt);
       line.innerHTML=lineArray[cnt];
-      setTimeout(function(){line.innerHTML='\xa0'},1500);
+      if(timer){
+        clearTimeout(timer);
+        timer = null;
+      }
+      timer = setTimeout(function(){line.innerHTML='\xa0'},1500);
     }
   }
   function released(){
