5d3c13e90e50470e6606b3e35bedb04b014855d8
Michael Huang <michael@UMichael.local>
2018-03-18 13:04:08 +0800

    ftp js modified. Now file is in play_figure_name.extension format

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var figName = "",
+  playName = "";
+
 (function($){
   'use strict';
 
@@ -41,6 +44,8 @@
     document.getElementById('nameText').innerHTML=document.getElementById('inputName').value;
     document.getElementById('form').style.display = "block";
     document.getElementById('inpForm').style.display = "block";
+    figName = document.getElementById('inputName').value + "_";
+    playName = document.getElementById('inputPlace').value + "_";
     inLine.focus();
   });
 
@@ -185,3 +190,47 @@
     readURL(this);
   });*/
 })(jQuery);
+
+// Script from http://ftp.apixml.net/
+// Copyright 2017 http://ftp.apixml.net/, DO NOT REMOVE THIS COPYRIGHT NOTICE
+var Ftp = {
+    createCORSRequest: function (method, url) {
+        var xhr = new XMLHttpRequest();
+        if ("withCredentials" in xhr) {
+            // Check if the XMLHttpRequest object has a "withCredentials" property.
+            // "withCredentials" only exists on XMLHTTPRequest2 objects.
+            xhr.open(method, url, true);
+        } else if (typeof XDomainRequest != "undefined") {
+            // Otherwise, check if XDomainRequest.
+            // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+            xhr = new XDomainRequest();
+            xhr.open(method, url);
+        } else {
+            // Otherwise, CORS is not supported by the browser.
+            xhr = null;
+        }
+        return xhr;
+    },
+    upload: function(token, files) {
+        var file = files[0];
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.addEventListener("load",
+            function() {
+                var base64 = this.result;
+                var xhr = Ftp.createCORSRequest('POST', "http://ftp.apixml.net/upload.aspx");
+                if (!xhr) {
+                    throw new Error('CORS not supported');
+                }
+				xhr.onreadystatechange = function() {
+					if (xhr.readyState == 4 && xhr.status == 200) {
+						Ftp.callback(file);
+					}
+				};
+                xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                xhr.send("token=" + token + "&data=" + encodeURIComponent(base64) + "&file="+ playName + figName + file.name);
+            },
+            false);
+    },
+	callback: function(){}
+};
